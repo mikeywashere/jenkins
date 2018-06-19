0f491abf1abcbcd667698cdfcb0471b433a4797c
Michael R. Schmidt <mikenging@hotmail.com>
2018-06-19 15:13:39 -0700

    major chopage and refactor to simplify

--- src/org/dfs/DfsLibrary.groovy
+++ src/org/dfs/DfsLibrary.groovy
@@ -1,11 +1,6 @@
 #!/usr/bin/groovy
 package org.dfs;
 
-enum OperatingSystem {
-    Windows,
-    Linux
-}
-
 class Constants {
     static final dollar = "\$"
     static final doubleQuote = '"'
@@ -15,107 +10,31 @@ class Constants {
     static final debug = "Debug"
 }
 
-class OperatingSystemConstants {
-    static final osName = "os.name"
-    static final windows = "windows"
-    static final linux = "linux"
+def String getContributorsScript() {
+    return "git log --pretty='{\"name\": \"%cn\", \"email\": \"%ce\"},' | sort | uniq | grep -v -E '@github.com';"
 }
 
-def OperatingSystem getOperatingSystem() {
-    return System.properties[OperatingSystemConstants.osName].toLowerCase().contains(OperatingSystemConstants.windows)
-        ? OperatingSystem.Windows
-        : OperatingSystem.Linux;
-}
-
-def toList(String value) {
-    return [value]
-}
-
-def toList(value) {
-    value ?: []
-}
-
-def String getContributorsScript(String filter) {
-    return getOperatingSystem() == OperatingSystem.Linux ? "git log --pretty='" + filter + "' | sort | uniq | tr '\\n' ','" : Constants.dollar + "data = (git log --pretty='%ce' | sort | Get-Unique); " + Constants.dollar + "data -join ';'";
-}
-
-def String getContributors(String filter) {
+def String getContributors() {
     def contributors = sh (
-        script: getContributorsScript(filter),
+        script: getContributorsScript(),
         returnStdout: true
     ).trim()
 
     if ( contributors == null || contributors.length() == 0 )
-        return ""
+        return "{}"
+
+    contributors = "[" + contributors + "]"
+
+    echo contributors
 
     return contributors;
 }
 
-def String cleanupContributors(String contributors) {
-    contributors = contributors.replace(',,', ',')
-    if ( contributors.endsWith(',') )
-        contributors = contributors.substring(0, contributors.length() - 1)
-    if ( contributors[0] == ',' )
-        contributors = contributors.substring(1)
+def Object getContributorNames() {
+    def jsonSlurper = new JsonSlurper()
+    def contributorsJson = getContributors();
 
-    return contributors;
-}
-
-def String getContributorsByEmail() {
-    def contributors = getContributors("%ce")
-
-    if ( contributors == null || contributors.length() == 0 )
-        return ""
-
-    contributors = contributors.replace('noreply@github.com', '')
-
-    return cleanupContributors(contributors);
-}
-
-def String getContributorsByName() {
-    def contributors = getContributors("%cn")
-
-    if ( contributors == null || contributors.length() == 0 )
-        return ""
-
-    contributors = contributors.replace('GitHub', '')
-
-    return contributors;
-}
-
-def dump(Object value) {
-    def filtered = ['class', 'active']
-
-    println value.properties
-        .sort{it.key}
-        .collect{it}
-        .join('\n')
-}
-
-def String getContributorsNames() {
-    def emailsTemp = getContributors("%ce")
-
-    println emailsTemp
-
-    def emails = toList(emailsTemp.split(","))
-
-    dump(emails)
-
-    def nameAndEmailsTemp = getContributors("%cn|%ce")
-    def nameAndEmails = toList(nameAndEmailsTemp.split(","))
-
-    dump(nameAndEmails)
-
-    def map = emails.collectEntries{ [(it.toLowerCase()):""] }
-    nameAndEmails.each {
-        def values = "${it}".split("|")
-        def name = values[0]
-        def email = values[1].toLowerCase()
-
-        def entry = emails[email]
-        if ( entry == null || entry == "" || entry.length() < name.length() ) {
-            map[email] = name
-        }
-    }
+    def object = jsonSlurper.parseText(contributorsJson)
+    return object;
 }
 
